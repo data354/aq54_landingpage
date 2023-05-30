221921544e3bb28d1322667c7fd6fb87da50de19
dykoffi <dykoffi2000@gmail.com>
2023-05-30 16:18:11 +0000

    Header Design ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
+import { Avatar, Button, Flex, Grid, Group, Text } from "@mantine/core";
+import logo from "./assets/logo.jpeg"
+import { IconPhoneCall } from "@tabler/icons-react";
+
+const Header = () => {
+  return <div className="h-20 px-5 flex items-center">
+    <Grid className="w-full">
+      <Grid.Col span={3}>
+        <Group>
+          <Avatar radius={"md"} src={logo} size={"lg"} alt="Data354 Logo" />
+          <Text fw={700} fz="lg">AQ54</Text>
+        </Group>
+      </Grid.Col>
+      <Grid.Col span={9} >
+        <Flex gap={50} align={"center"} justify={"end"}>
+          <Text>Qualité de l'air</Text>
+          <Text>Projet AQ54</Text>
+          <Text>Données</Text>
+          <Button leftIcon={<IconPhoneCall />} variant="filled" className="bg-red-700 hover:bg-red-800">Contactz-nous</Button>
+        </Flex>
+      </Grid.Col>
+    </Grid>
+  </div>
+}
 
 const App = () => {
-  return <div className="text-xl font-bold">Demain est dimanche</div>
+  return (
+    <>
+      <Header />
+    </>
+
+  );
 }
 
 export default App;
