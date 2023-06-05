d254fc63c6585774b4df6c2397f1654b6d13b116
dykoffi <dykoffi2000@gmail.com>
2023-06-05 15:56:34 +0000

    Footer, Lancement, Map oks

---
+++ src/data/sensors.ts
@@ -0,0 +1,24 @@
+import fondSmart188 from "../assets/sensor188_3.jpeg"
+import fondSmart189 from "../assets/sensor189_2.jpeg"
+
+
+export default [{
+  name: "SMART189",
+  emplacement: "Pharmarcie Ministre",
+  description: "Près d'une rue non bitumée",
+  picture: fondSmart189,
+  location: {
+    lat: 5.365315914154053,
+    lng: -3.9578371047973633
+  }
+},
+{
+  name: "SMART188",
+  emplacement: "Pharmarcie du Bonheur",
+  description: "Pres d'une rue bitumée",
+  picture: fondSmart188,
+  location: {
+    lat: 5.369297504425049,
+    lng: -3.958930253982544
+  }
+}]
